5a8ce0a04f477dc24ed98ba98c891a7078bdaad3
ehpc <ehpc@ehpc.io>
2020-10-22 12:13:11 +0300

    Fix

--- index.js
+++ index.js
@@ -1,14 +1,3 @@
-function countVowels(text) {
-  return text.match(/[ауоыиэяюёе]/ig).length;
-}
-
-function splitWords(text) {
-  return text.match(/[а-я]+/ig);
-}
-
-function removePunctuation(text) {
-  return text.replace(/[^а-я]/ig, '');
-}
 
 module.exports = {
   countVowels,
