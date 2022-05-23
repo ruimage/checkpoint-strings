110d670bc1482ffa5318c06fc55d6ccafa7538fa
Anton Valkovsky <ruizave@gmail.com>
2022-05-23 10:16:32 +0300

    решение

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
+const countVowels = (text) => text.match(/[АЕЁИОУЫЭЮЯаеёиоуыэюя]/gi).length;
+
+const splitWords = (text) => text
+  .split(/[^АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя]/gm)
+  .filter((v) => v !== '');
+
+const removePunctuation = (text) => text
+  .replace(/[^АБВГДЕЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя]/gm, '');
 
 module.exports = {
   countVowels,
